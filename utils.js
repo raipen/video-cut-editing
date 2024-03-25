980a57cd498a7c98db46a202838cfd7eedcbe232
raipen <raipen@knu.ac.kr>
2024-03-26 01:24:03 +0900

    feat: read csv utils

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 import { spawn } from 'child_process';
+import fs from 'fs';
 
 export const checkFfmpegInstalled = async () => {
     return new Promise((resolve, reject) => {
@@ -13,5 +14,45 @@ export const checkFfmpegInstalled = async () => {
                 resolve(false);
             }
         });
+    });
+};
+
+export const fileExists = async (path) => {
+    return new Promise((resolve, reject) => {
+        fs.access(path, fs.constants.F_OK, (err) => {
+            if (err) {
+                resolve(false);
+            } else {
+                resolve(true);
+            }
+        });
+    });
+};
+
+export const getCSVData = async (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                const lines = data.split('\n');
+                const [header, ...rows] = lines;
+                const keys = header.split(',');
+                const result = rows.map((row) => {
+                    const values = row.split(',');
+                    return keys.reduce((acc, key, i) => {
+                        acc[key] = values[i];
+                        return acc;
+                    }, {});
+                });
+                resolve(result);
+            }
+        });
+    });
+};
+
+export const validateCSV = async (csvData) => {
+    return csvData.every((row) => {
+        return row.hasOwnProperty('video') && row.hasOwnProperty('start') && row.hasOwnProperty('end');
     });
 };
